bde4a6291109927cb8c2965d611c6a6db6dbc5e8
‘mali_mac’ <mali1711@163.com>
2020-01-31 20:09:48 +0800

    填写订单添加接口

---
+++ application/api/controller/ManageTime.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\api\controller;
+
+use think\Controller;
+use think\Request;
+
+class ManageTime extends Controller{
+    //获取日期
+    public function getInitial()
+    {
+        $cuttime = time();
+        $time = $this->_gettimeToday();
+        for ($i=0;$i<=5;$i++){
+            $cuttdate[] = date("m-d",$cuttime+$i*24*60*60);
+        }
+        $data = array();
+        $data['date'] = $cuttdate;
+        $data['time'] = $time;
+        return $data;
+    }
+
+    /**
+     * 用户通过点击获取时间
+     */
+    public function getclickTime(Request $request)
+    {
+       $data = $request->get('date');
+       $date = date('Y-').$data;
+       if ($date==date('Y-m-d')){
+           $time = $this->_gettimeToday();
+       }else{
+           $time = $this->_gettimeToOtherDay($date);
+       }
+       return $time;
+    }
+
+    /**
+     * 获取当天日期
+     * @param null $time
+     * @return array|null
+     *
+     * {st_id38: "undefined", pic_1https://dongyi.sir6.cn/uploads/project/20191228/90ce28d1fb2341a1cac05bd5e37e623a.jpg: "undefined", pr_id: "40", subscribetime: "01-29 17:00"}
+     */
+    public function _gettimeToday()
+    {
+        $cuttime = time();
+        $todaystart = strtotime(date("Y-m-d")."08:00:00",time());
+        $todayend = strtotime(date("Y-m-d")."23:59:59",time());
+        for ($i=1;$i<=23;$i++){
+            if($cuttime+$i*60*60<=$todayend and $cuttime+$i*60*60>=$todaystart){
+                $time[] =   date("H",$cuttime+$i*60*60);
+            }
+        }
+        return $time;
+    }
+    /**
+     * 获取其他时间的日期
+     */
+    public function _gettimeToOtherDay($date)
+    {
+        $cuttime = strtotime($date,time());
+        $todaystart = strtotime($date."08:00:00",time());
+        $todayend = strtotime($date."23:59:59",time());
+        for ($i=1;$i<=23;$i++){
+            if($cuttime+$i*60*60<=$todayend and $cuttime+$i*60*60>=$todaystart){
+                $time[] =   date("H",$cuttime+$i*60*60);
+            }
+        }
+        return $time;
+    }
+}
---
+++ application/api/controller/Order.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace app\api\controller;
+
+use think\Controller;
+use think\Db;
+use think\Request;
+
+class Order extends Controller
+{
+    /**
+     * 显示所有订单列表
+     *
+     * @return \think\Response
+     */
+    public function getindex()
+    {
+        //
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function getcreate()
+    {
+        //
+    }
+
+    /**
+     * 保存用户提交的订单信息
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function postsave(Request $request)
+    {
+        //
+        //$data = $request->post();
+
+        $data['user_id'] = $request->post('user_id');
+        $data['st_id'] = $request->post('st_id');
+        $data['pr_id'] = $request->post('pr_id');
+        $data['subtime'] = strtotime(date('Y').'-'.$request->post('subtime'),time());//预约时间
+        $st_info = Db::table('staff')->where('st_id',$data['st_id'])->find();
+        $pr_info = Db::table('project')->where('pr_id',$data['pr_id'])->find();
+        $data['st_info'] = json_encode($st_info);
+        $data['pr_info'] = json_encode($pr_info);
+        $data['add_purchase_num'] = 0;//加购数量
+        $data['add_purchase_price'] = $pr_info['add_purchase_price'];//加购单价
+        $data['add_purchase_tprice'] = $data['add_purchase_num']*$data['add_purchase_price'];//加购总价
+        $data['subsidy'] = 0;//路费补助
+        $data['total_price'] = $pr_info['price'];//订单总价,不含加价
+        $res = Db::table('order')->insert($data);
+        if($res){
+            $result = array(
+                'msg'=>'添加成功',
+                'err'=>0,
+            );
+        }else{
+            $result = array(
+                'msg'=>'添加失败',
+                'err'=>1,
+            );
+        }
+
+        return $res;
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
