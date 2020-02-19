9a5b6ec9a37c1c77344fff46e1823d15dc8a0f1c
‘mali_mac’ <mali1711@163.com>
2020-02-19 19:38:41 +0800

    添加技师端

---
+++ application/staff/controller/Order.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * 订单中心
+ */
+namespace app\staff\controller;
+
+use think\Controller;
+use think\Request;
+
+class Order extends Controller{
+
+    /**
+     * 订单列表
+     * @param Request $request
+     */
+    public function getList(Request $request)
+    {
+        
+    }
+
+    /**
+     * 接单
+     * @param Request $request
+     */
+    public function getreceipt(Request $request)
+    {
+        
+    }
+
+
+    /**
+     * 订单详情
+     * @param Request $request
+     */
+    public function getdetail(Request $request)
+    {
+        
+    }
+    
+    /**
+     * 订单通知
+     * @param Request $request
+     */
+    public function getnotice(Request $request)
+    {
+        
+    }
+}
---
+++ application/staff/controller/Staff.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * 个人中心
+ */
+namespace app\staff\controller;
+
+use think\Controller;
+use think\Db;
+use think\Request;
+
+class Staff extends Controller{
+
+    /**
+     * 登陆
+     * @param Request $request
+     */
+    public function postLogin(Request $request)
+    {
+        $where = $request->post('');
+        $where['passwd'] = md5('dongyi'.$request->post('passwd'));
+        $staffInfo = Db::table('staff')->where($where)->find();
+        if($staffInfo){
+           return returnApi('0','登陆成功',$staffInfo);
+        }else{
+           return returnApi('10001','账号或者密码错误',$staffInfo);
+        }
+    }
+
+
+    /**
+     * 找回密码
+     * @param Request $request
+     */
+    public function postforget(Request $request)
+    {
+        $where['iphone'] = $request->post('iphone');
+        $data['passwd'] = md5('dongyi'.$request->post('passwd'));
+        $res = Db::table('staff')->where($where)->update($data);
+        if($res){
+            return returnApi('0','密码修改成功','');
+        }else{
+            return returnApi('10002','密码修改失败','');
+        }
+    }
+
+    /**
+     * 暂停服务
+     * @param Request $request
+     */
+    public function getleave(Request $request)
+    {
+        $where = $request->get('');
+        $data['status']=0;
+        $res = Db::table('staff')->where($where)->update($data);
+        if($res){
+            return returnApi('0','休假成功','');
+        }else{
+            return returnApi('10003','休假失败','');
+        }
+    }
+
+    /**
+     * 结束休假
+     * @param Request $request
+     */
+    public function getleaveOff(Request $request)
+    {
+        $where = $request->get('');
+        $data['status']=1;
+        $res = Db::table('staff')->where($where)->update($data);
+        if($res){
+            return returnApi('0','结束休假成功','');
+        }else{
+            return returnApi('10004','结束休假失败','');
+        }
+    }
+
+    /**
+     * 查看用户信息
+     * @param Request $request
+     */
+    public function getshow(Request $request)
+    {
+        $res = Db::table('staff')->where($request->get(''))->find();
+        return returnApi(0,'信息获取成功',$res);
+    }
+
+    /**
+     * 修改个人信息
+     * @param Request $request
+     */
+    public function postsave(Request $request)
+    {
+        $where['st_id'] = $request->post('st_id');
+        $data = $request->post('');
+        unset($data['st_id']);
+        $res = Db::table('staff')->where($where)->update($data);
+        if ($res){
+            return returnApi('0','用户信息更新成功','');
+        }else{
+            return returnApi('10005','用户信息更新成功失败','');
+        }
+    }
+}
